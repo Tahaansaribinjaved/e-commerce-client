c2c77abe17a74fd057648a6dc876321b61391e8c
Tahaanaribinjaved <iamtahaansari293@gmail.com>
2024-10-14 23:03:20 +0500

    Day 3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div 
-    className='text-red-400 bg-black'
+    className='text-red-400 bg-black '
     >Well come to home page </div>
   )
 }

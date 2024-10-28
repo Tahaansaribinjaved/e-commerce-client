37f733d05a2c194bd0dd238cc20fb063c232dd4e
Tahaanaribinjaved <iamtahaansari293@gmail.com>
2024-10-28 22:18:26 +0500

    start

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,14 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", // Add paths to your component files
+    'node_modules/preline/dist/*.js',
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require("daisyui"),
+    require('preline/plugin'),
     // You can add Tailwind CSS animations here if needed
   ],
 };
